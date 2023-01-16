11292c5000e0e9c6d2adcda83d3e34a5c4451131
asmaajameel <asmaagazi2608@gmail.com>
2023-01-15 21:51:48 -0500

    jan 15,2023, push#2

--- app/src/main/java/com/example/garagesale/LoginActivity.java
+++ app/src/main/java/com/example/garagesale/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.garagesale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    Button Login;
+    TextView Register;
+    TextView ForgotPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Register = findViewById(R.id.tVNewRegister);
+        ForgotPass = findViewById(R.id.tVForgotpassword);
+
     }
+
+    public void NewRegister(View view) {
+        Intent RegisterActivityIntent = new Intent(this, RegisterActivity.class);
+        startActivity(RegisterActivityIntent);
+    }
+
+    public void Forgotpassword(View view) {
+        Intent intent = new  Intent(this, ResetPasswordActivity.class);
+        startActivity(intent);
+    }
+//
+//    private void startActivities(Intent registerActivityIntent) {
+//    }
+
+
 }
